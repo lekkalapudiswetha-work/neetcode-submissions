c78227a207444566c1b890815e86ad00710db1b7
Swetha Lekkalapudi <lekkalapudiswetha.work@gmail.com>
2026-07-21 19:26:53 -0400

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # i = 0
+        # while i <= len(nums):
+        #     maxRange = min(len(nums),i+k+1)
+        #     for j in range(i+1,maxRange):
+        #         if j <= len(nums) and nums[i] == nums[j]:
+        #             return True
+        #     i+=1
+        
+        # return False
+
+        map = {}
+
+        for i in range(len(nums)):
+            if nums[i] in map and i-map[nums[i]] <= k:
+                return True
+            map[nums[i]] = i
+
+        return False   
